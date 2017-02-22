cca1b890cc29ea92430aba78f81f408518e40a49
Dawid Dul <dawid.dul@gmail.com>
2017-02-22 21:48:39 +0100

    Included python3 libraries.

--- system_setup.sh
+++ system_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-# Make sure only root can run our script
+# Making sure that root is running this script.
 if [ $EUID -ne 0 ]; then
-   echo "This script must be run as root" 1>&2
+   echo "This script must be run as root." 1>&2
    exit 1
 fi
 
@@ -15,15 +15,39 @@ function agf {
     apt-get --assume-yes install $1 
 }
 
-
 # List of packages to install
 packageList=( git \
               python3 \
               python3-dev \
-              python3-pip )
+              python3-pip \
+              gedit \
+              vim )
 
 
 for p in "${packageList[@]}"
 do
     agf $p
 done
+
+
+# A function that will invoce the pip3 command.
+# p3f stands for pip3 function
+function p3f {
+    pip3 install -U $1
+}
+
+# List of python libraries
+
+pythonLibraries=( matplotlib \
+                  numpy \
+                  scipy \
+                  scikit-learn \
+                  pandas )
+
+for l in "${pythonLibraries[@]}"
+do
+    p3f $l
+done
+
+
+
